442a9de4e7a18beb1f6a8884ae63fa08606a132d
eylens <eymasac@gmail.com>
2022-02-10 21:25:06 -0500

    the code was adjusted and html was changed

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,17 @@
 // Listen for changes on each input checkbox tag
 
-let check_box = {}
 $(document).ready(function () {
-  $('input:checkbox').change(function () {
-    if ($(this).is(':check_box')) {
-      check_box[$(this).data('id')] = $(this).data('name');
+  let check_box = {}
+  $(document).on('change', 'input[type="checkbox"]', function () {
+    if (this.checked) {
+      check_box[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      delete check_box[$(this).data('id')];
+      delete check_box[$(this).attr('data-id')]
     }
-  })
-  $('div.amenities h4').html(function () {
-    let listUp = [];
-    Object.keys(check_box).forEach(function (key) {
-      listUp.push(check_box[key]);
-    });
+    let listUp = [] = Object.values(check_box);
     if (listUp.length > 0) {
-      return $('div.amenities h4').text(listUp.join(', '));;
+      $('div.amenities h4').text(listUp).join(', ');
     }
-    return ('empty;')
+    $('div.amenities h4').html('&nbsp;');
   });
 });
