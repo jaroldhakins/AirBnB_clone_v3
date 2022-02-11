dc2958bae60db0cc5b8c63b966a9770950c10b80
eylens <eymasac@gmail.com>
2022-02-11 12:14:03 -0500

    include the get method inside the document.ready

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,23 +8,20 @@ $(document).ready(function () {
     } else {
       delete check_box[$(this).attr('data-id')]
     }
-    let listUp = [] = Object.values(check_box);
+    let listUp = Object.values(check_box);
     if (listUp.length > 0) {
       $('div.amenities h4').text(listUp).join(', ');
     }
     $('div.amenities h4').html('&nbsp;');
   });
+
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, function (data, status) {
+    console.log(data);
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
 });
-
-// Request and modify the style by jQuery
-
-const url = 'http://0.0.0.0:5001/api/v1/status/';
-$.get(url, function (data, status) {
-  console.log(data);
-  if (data.status === 'OK') {
-    $('div#api_status').addClass('available');
-  } else {
-    $('div#api_status').removeClass('available');
-  }
-});
-
